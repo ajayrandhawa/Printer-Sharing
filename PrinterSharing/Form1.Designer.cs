dc0224d40ba81629a71f46ff78b32a00fd89182d
Ajay Randhawa <ajayrandhawartg@gmail.com>
2023-09-11 21:20:51 +0530

    Initial commit

---
+++ PrinterSharing/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PrinterSharing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LocalPrinterListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Local Printers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Remote Printers";
+            // 
+            // LocalPrinterListView
+            // 
+            this.LocalPrinterListView.FullRowSelect = true;
+            this.LocalPrinterListView.LabelWrap = false;
+            this.LocalPrinterListView.Location = new System.Drawing.Point(12, 45);
+            this.LocalPrinterListView.MultiSelect = false;
+            this.LocalPrinterListView.Name = "LocalPrinterListView";
+            this.LocalPrinterListView.Size = new System.Drawing.Size(373, 208);
+            this.LocalPrinterListView.TabIndex = 3;
+            this.LocalPrinterListView.UseCompatibleStateImageBehavior = false;
+            this.LocalPrinterListView.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 591);
+            this.Controls.Add(this.LocalPrinterListView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Printer Sharing 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView LocalPrinterListView;
+    }
+}
+
---
+++ PrinterSharing/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Printing;
+using System.Management;
+
+namespace PrinterSharing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create columns for the ListView
+            LocalPrinterListView.Columns.Add("Printer");
+            LocalPrinterListView.Columns.Add("Sharing");
+            LocalPrinterListView.Columns.Add("Status");
+
+            LocalPrinterListView.View = View.Details;
+
+            int totalWidth = LocalPrinterListView.Width;
+            int columnWidth = totalWidth / 3; // 33% of the total width
+
+            // Assuming you want to set the width of the first column to 33%
+            if (LocalPrinterListView.Columns.Count > 0)
+            {
+                LocalPrinterListView.Columns[0].Width = columnWidth;
+                LocalPrinterListView.Columns[1].Width = columnWidth;
+                LocalPrinterListView.Columns[2].Width = columnWidth - 5;
+            }
+
+            // Add the list of printers to a ListBox control
+            // Get the list of installed local printers
+            string[] printerList = PrinterSettings.InstalledPrinters.Cast<string>().ToArray();
+
+            // Add the list of printers to the ListView
+            foreach (string printerName in printerList)
+            {
+
+                // Check if the printer is shared
+                string sharingStatus = IsPrinterShared(printerName) ? "Shared" : "Not Shared";
+                string status = GetPrinterStatus(printerName);
+
+                ListViewItem printerItem = new ListViewItem(printerName);
+                printerItem.SubItems.Add(sharingStatus);
+                printerItem.SubItems.Add(status);
+                LocalPrinterListView.Items.Add(printerItem);
+            };
+        }
+
+
+        private bool IsPrinterShared(string printerName)
+        {
+            bool isShared = false;
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Printer"))
+            {
+                foreach (ManagementObject printer in searcher.Get())
+                {
+                    string name = printer["Name"].ToString();
+                    if (name == printerName)
+                    {
+                        isShared = Convert.ToBoolean(printer["Shared"]);
+                        break;
+                    }
+                }
+            }
+            return isShared;
+        }
+
+
+        private string GetPrinterStatus(string printerName)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Printer"))
+            {
+                foreach (ManagementObject printer in searcher.Get())
+                {
+                    string name = printer["Name"].ToString();
+                    if (name == printerName)
+                    {
+                        int status = Convert.ToInt32(printer["PrinterStatus"]);
+                        if (status == 3) // Printer is idle
+                        {
+                            return "Online";
+                        }
+                    }
+                }
+            }
+            return "Offline";
+        }
+
+
+    }
+    
+}
